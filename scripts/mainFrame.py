37141fe4cdc8df0254babecaa1566d8213e2a4e8
devthylui <devthylui@gmail.com>
2025-12-28 22:47:23 +0800

    Update mainFrame.py

--- scripts/mainFrame.py
+++ scripts/mainFrame.py
@@ -26,7 +26,7 @@ class mainFrame(QMainWindow):
     def __init__(self, switch_callback=None):
         super().__init__()
 
-        pix = QPixmap(resource_path("resources/Logo 1.png"))
+        pix = QPixmap(resource_path("resources/LOGO.png"))
         
         self.setObjectName("mainFrame")
         self.resize(800, 480)
